514c83e2a1ff5ac4ca24cbe122cefa6cca68fe86
JauniusPinelis <JauniusPinelis@gmail.com>
2020-03-29 14:24:12 +0300

    Connected react front with api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { Component, ReactNode } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+interface SdnEntry {
+  uid: number;
+  lastName: string;
+  sdnType: string;
+  LastModified: Date;
+}
+
+interface State {
+  sdnEntries: SdnEntry[];
+}
+
+class App extends Component {
+  state: State = {
+    sdnEntries: []
+  };
+  componentDidMount() {
+    axios.get(`/data`).then(res => {
+      this.setState({ sdnEntries: res.data.sdnEntries });
+    });
+  }
+  render(): ReactNode {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.sdnEntries.map(entry => (
+            <div>
+              <div>{entry.uid}</div>
+              <div>{entry.lastName}</div>
+              <div>{entry.sdnType}</div>
+              <div>{entry.LastModified}</div>
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
